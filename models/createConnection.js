22042883ae4c9127b918e0fb04fd7f544ba915be
Côme Plantin-Carrenard <120224883+comus3@users.noreply.github.com>
2023-12-08 15:18:22 +0100

    Update createConnection.js

--- models/createConnection.js
+++ models/createConnection.js
@@ -1,23 +1,21 @@
 import bluebird from 'bluebird';
-
-const fs = require('fs');
+import fs from 'fs';
+import mysql from "mysql2";
 
 // Read the content of pwd.txt synchronously
-try {
     const passwordContent = fs.readFileSync('pwd.txt', 'utf8');
-    
-    var mysql = require("mysql");
     //connection base de donnees
     var connection = mysql.createConnection({
         host : 'localhost',
         user : 'root',
         password : passwordContent,
-        database : 'WRITE YOUR DATA BASE NAME HERE',
+        database : 'YOUR DATA BASE EHRE',
         Promise: bluebird
     });
+    export default connection
 
+    try {
     connection.connect(function(error){if (error) console.log(error);});
-    module.exports = connection;
 } catch (err) {
   console.error('Error reading pwd.txt:', err);
 }
