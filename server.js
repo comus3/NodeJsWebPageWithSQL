4e4617e43cca88499457837bdac0aa0d6206bc43
comus3 <come.plantin.carrenard@gmail.com>
2023-12-08 11:15:30 +0100

    server.js creation

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+app.use(express.static("public"));
+
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended:true}));
+
+
+app.use(express.static('public'));
+
+app.post('/', (request,response)=>{
+
+    //do something
+})
+
+
+app.get('/',(request,response)=>{
+    //do something
+})
+
+
+app.listen(3000, function(){
+    console.log("Server ok");
+});
+
+
+
+
